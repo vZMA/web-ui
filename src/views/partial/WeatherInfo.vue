fa1e9cfc8fca9e4d58e38f8b050f56b7255538c3
FriedrichKayak <jason.cochran@gmail.com>
2022-01-03 19:55:15 -0500

    fixed bracket compile issue

--- src/views/partial/WeatherInfo.vue
+++ src/views/partial/WeatherInfo.vue
@@ -51,299 +51,356 @@
 </template>
 
 <script>
-import {zabApi} from '@/helpers/axios.js';
-import parse from 'metar-parser';
+import { zabApi } from "@/helpers/axios.js";
+import parse from "metar-parser";
 
 export default {
-	data() {
-		return {
-			icao: ['KMIA', 'KFLL', 'KTPA', 'KRSW', 'KPBI', 'KEYW'],
-			stations: {
-				KMIA: {
-					icao: "KMIA",
-					fullName: "Miami International Airport",
-					metar: null,
-					parsedMetar: null,
-					configs: {
-						landing: {
-							WD: '26R/30/26L/27',
-							ED: '9/12/8L/8R',
-							WN: '26L',
-							EN: '8R/9',
-						},
-						departing: {
-							WD: '26L/27/26R/30',
-							ED: '8R/8L/9/12',
-							WN: '26L/ 27',
-							EN: '8R/9',
-						}
-					},
-					getLanding: function() {
-						if(this.parsedMetar.wind.speedKt < 5) {
-							if(this.parsedMetar.time.hour >= 6 && this.parsedMetar.time.hour <= 23) {
-								return this.configs.landing.ED;
-							} else {
-								return this.configs.landing.EN;
-							}
-						} else {
-							if(this.parsedMetar.wind.direction > 180 && this.parsedMetar.wind.direction <=359) {
-								//west ops
-								if(this.parsedMetar.time.hour >= 6 && this.parsedMetar.time.hour <= 23) {
-									return this.configs.landing.WD;
-								} else {
-									return this.configs.landing.WN;
-								}
-							} else {
-								if(this.parsedMetar.time.hour >= 6 && this.parsedMetar.time.hour <= 23) {
-									return this.configs.landing.ED;
-								} else {
-									return this.configs.landing.EN;
-								}
-							}
-						}
-					},
-					getDeparting: function() {
-						if(this.parsedMetar.wind.speedKt < 5) {
-							if(this.parsedMetar.time.hour >= 6 && this.parsedMetar.time.hour <= 23) {
-								return this.configs.departing.ED;
-							} else {
-								return this.configs.departing.EN;
-							}
-						} else {
-							if(this.parsedMetar.wind.direction > 180 && this.parsedMetar.wind.direction <=359) {
-								//west ops
-								if(this.parsedMetar.time.hour >= 6 && this.parsedMetar.time.hour <= 23) {
-									return this.configs.departing.WD;
-								} else {
-									return this.configs.departing.WN;
-								}
-							} else {
-								if(this.parsedMetar.time.hour >= 6 && this.parsedMetar.time.hour <= 23) {
-									return this.configs.departing.ED;
-								} else {
-									return this.configs.departing.EN;
-								}
-							}
-						}
-					}
-				} ,
-				KFLL: {
-					icao: "KFLL",
-					fullName: "Ft Lauderdale Hollywood International Airport",
-					metar: null,
-					parsedMetar: null,
-					configs: {
-						landing: {
-							E: '10L/10R',
-							W: '28L/28R',
-						},
-						departing: {
-							E: '10L/10R',
-							W: '28L/28R',
-						}
-					},
-					getLanding: function() {
-						if(this.parsedMetar.wind.speedKt < 5) {
-							return this.configs.landing.E;
-						} else {
-							if(this.parsedMetar.wind.direction > 180 && this.parsedMetar.wind.direction <=359) {
-								return this.configs.landing.W;
-							} else {
-								return this.configs.landing.E;
-							}
-						}
-					},
-					getDeparting: function() {
-						if(this.parsedMetar.wind.speedKt < 5) {
-							return this.configs.departing.E;
-						} else {
-							if(this.parsedMetar.wind.direction > 180 && this.parsedMetar.wind.direction <=359) {
-								return this.configs.departing.W;
-							} else {
-								return this.configs.departing.E;
-							}
-						}
-					}
-				},
-				KTPA: {
-					icao: "KTPA",
-					fullName: "Tampa International Airport",
-					metar: null,
-					parsedMetar: null,
-					configs: {
-						landing: {
-							N: '1L/1R',
-							S: '19L/19R',
-						},
-						departing: {
-							N: '1L/1R',
-							S: '19L/19R',
-						}
-					},
-					getLanding: function() {
-						if(this.parsedMetar.wind.speedKt < 5) {
-							return this.configs.landing.N;
-						} else {
-							if(this.parsedMetar.wind.direction > 90 && this.parsedMetar.wind.direction <= 270) {
-								return this.configs.landing.S;
-							} else {
-								return this.configs.landing.N;
-							}
-						}
-					},
-					getDeparting: function() {
-						if(this.parsedMetar.wind.speedKt < 5) {
-							return this.configs.departing.N;
-						} else {
-							if(this.parsedMetar.wind.direction > 90 && this.parsedMetar.wind.direction <= 270) {
-								return this.configs.departing.S;
-							} else {
-								return this.configs.departing.N;
-							}
-						}
-					}
-				},
-				KRSW: {
-					icao: "KRSW",
-					fullName: "Southwest Florida International Airport",
-					metar: null,
-					parsedMetar: null,
-					configs: {
-						landing: {
-							E: '6',
-							W: '24'
-						},
-						departing: {
-							E: '6',
-							W: '24'
-						}
-					},
-					getLanding: function() {
-						if(this.parsedMetar.wind.speedKt < 5) {
-							return this.configs.landing.E;
-						} else {
-							if(this.parsedMetar.wind.direction >= 0 && this.parsedMetar.wind.direction < 180) {
-								return this.configs.landing.E;
-							} else {
-								return this.configs.landing.W;
-							}
-						}
-					},
-					getDeparting: function() {
-						if(this.parsedMetar.wind.speedKt < 5) {
-							return this.configs.departing.E;
-						} else {
-							if(this.parsedMetar.wind.direction >= 0 && this.parsedMetar.wind.direction < 180) {
-								return this.configs.departing.E;
-							} else {
-								return this.configs.departing.W;
-							}
-						}
-					}
-				},
-				KPBI: {
-					icao: "KPBI",
-					fullName: "Palm Beach International Airport",
-					metar: null,
-					parsedMetar: null,
-					configs: {
-						landing: {
-							E: '10L/14',
-							W: '32/28R',
-						},
-						departing: {
-							E: '14/10L',
-							W: '28R/32',
-						}
-					},
-					getLanding: function() {
-						if(this.parsedMetar.wind.speedKt < 5) {
-							return this.configs.landing.E;
-						} else {
-							if(this.parsedMetar.wind.direction >= 0 && this.parsedMetar.wind.direction < 180) {
-								return this.configs.landing.E;
-							} else {
-								return this.configs.landing.W;
-							}
-						}
-					},
-					getDeparting: function() {
-						if(this.parsedMetar.wind.speedKt < 5) {
-							return this.configs.departing.E;
-						} else {
-							if(this.parsedMetar.wind.direction >= 0 && this.parsedMetar.wind.direction < 180) {
-								return this.configs.departing.E;
-							} else {
-								return this.configs.departing.W;
-							}
-						}
-					},
-					KEYW: {
-					icao: "KEYW",
-					fullName: "Key West International Airport",
-					metar: null,
-					parsedMetar: null,
-					configs: {
-						landing: {
-							E: '9',
-							W: '27',
-						},
-						departing: {
-							E: '9',
-							W: '27',
-						}
-					},
-					getLanding: function() {
-						if(this.parsedMetar.wind.speedKt < 5) {
-							return this.configs.landing.E;
-						} else {
-							if(this.parsedMetar.wind.direction >= 0 && this.parsedMetar.wind.direction < 180) {
-								return this.configs.landing.E;
-							} else {
-								return this.configs.landing.W;
-							}
-						}
-					},
-					getDeparting: function() {
-						if(this.parsedMetar.wind.speedKt < 5) {
-							return this.configs.departing.E;
-						} else {
-							if(this.parsedMetar.wind.direction >= 0 && this.parsedMetar.wind.direction < 180) {
-								return this.configs.departing.E;
-							} else {
-								return this.configs.departing.W;
-							}
-						}
-					}
-				}
-			},
-			numStationsLoaded: 0
-		};
-	},
-	async mounted() {
-		await this.getWeatherForAirports();
-	},
-	methods: {
-		async getWeatherForAirports() {
-			for (const station of this.icao) {
-				const { data } = await zabApi.get(`/ids/stations/${station}`);
-				this.stations[station].metar = data.metar;
-				this.stations[station].parsedMetar = parse(data.metar);
-				this.numStationsLoaded++;
-
-			}
-
-		},
-		formatWind: function(station) {
-			if(station.parsedMetar.wind.speedKt < 4) return 'Calm';
-			else if(!('speedKt' in station.parsedMetar.wind)) return 'Unknown';
-			const paddedWind = `0${station.parsedMetar.wind.direction}`.slice(-3);
-			return `${paddedWind}@${station.parsedMetar.wind.speedKt}${(station.parsedMetar.wind.gust?`G${station.parsedMetar.wind.gust}`:'')}`;
-		},
-		getConditions: function(station) {
-			return (station.parsedMetar.visibility.miles > 3) ? `<i class="material-icons weather_icon">wb_sunny</i>VFR` : `<i class="material-icons weather_icon">wb_cloudy</i>IFR`;
-		}
-	}
+  data() {
+    return {
+      icao: ["KMIA", "KFLL", "KTPA", "KRSW", "KPBI", "KEYW"],
+      stations: {
+        KMIA: {
+          icao: "KMIA",
+          fullName: "Miami International Airport",
+          metar: null,
+          parsedMetar: null,
+          configs: {
+            landing: {
+              WD: "26R/30/26L/27",
+              ED: "9/12/8L/8R",
+              WN: "26L",
+              EN: "8R/9",
+            },
+            departing: {
+              WD: "26L/27/26R/30",
+              ED: "8R/8L/9/12",
+              WN: "26L/ 27",
+              EN: "8R/9",
+            },
+          },
+          getLanding: function () {
+            if (this.parsedMetar.wind.speedKt < 5) {
+              if (
+                this.parsedMetar.time.hour >= 6 &&
+                this.parsedMetar.time.hour <= 23
+              ) {
+                return this.configs.landing.ED;
+              } else {
+                return this.configs.landing.EN;
+              }
+            } else {
+              if (
+                this.parsedMetar.wind.direction > 180 &&
+                this.parsedMetar.wind.direction <= 359
+              ) {
+                //west ops
+                if (
+                  this.parsedMetar.time.hour >= 6 &&
+                  this.parsedMetar.time.hour <= 23
+                ) {
+                  return this.configs.landing.WD;
+                } else {
+                  return this.configs.landing.WN;
+                }
+              } else {
+                if (
+                  this.parsedMetar.time.hour >= 6 &&
+                  this.parsedMetar.time.hour <= 23
+                ) {
+                  return this.configs.landing.ED;
+                } else {
+                  return this.configs.landing.EN;
+                }
+              }
+            }
+          },
+          getDeparting: function () {
+            if (this.parsedMetar.wind.speedKt < 5) {
+              if (
+                this.parsedMetar.time.hour >= 6 &&
+                this.parsedMetar.time.hour <= 23
+              ) {
+                return this.configs.departing.ED;
+              } else {
+                return this.configs.departing.EN;
+              }
+            } else {
+              if (
+                this.parsedMetar.wind.direction > 180 &&
+                this.parsedMetar.wind.direction <= 359
+              ) {
+                //west ops
+                if (
+                  this.parsedMetar.time.hour >= 6 &&
+                  this.parsedMetar.time.hour <= 23
+                ) {
+                  return this.configs.departing.WD;
+                } else {
+                  return this.configs.departing.WN;
+                }
+              } else {
+                if (
+                  this.parsedMetar.time.hour >= 6 &&
+                  this.parsedMetar.time.hour <= 23
+                ) {
+                  return this.configs.departing.ED;
+                } else {
+                  return this.configs.departing.EN;
+                }
+              }
+            }
+          },
+        },
+        KFLL: {
+          icao: "KFLL",
+          fullName: "Ft Lauderdale Hollywood International Airport",
+          metar: null,
+          parsedMetar: null,
+          configs: {
+            landing: {
+              E: "10L/10R",
+              W: "28L/28R",
+            },
+            departing: {
+              E: "10L/10R",
+              W: "28L/28R",
+            },
+          },
+          getLanding: function () {
+            if (this.parsedMetar.wind.speedKt < 5) {
+              return this.configs.landing.E;
+            } else {
+              if (
+                this.parsedMetar.wind.direction > 180 &&
+                this.parsedMetar.wind.direction <= 359
+              ) {
+                return this.configs.landing.W;
+              } else {
+                return this.configs.landing.E;
+              }
+            }
+          },
+          getDeparting: function () {
+            if (this.parsedMetar.wind.speedKt < 5) {
+              return this.configs.departing.E;
+            } else {
+              if (
+                this.parsedMetar.wind.direction > 180 &&
+                this.parsedMetar.wind.direction <= 359
+              ) {
+                return this.configs.departing.W;
+              } else {
+                return this.configs.departing.E;
+              }
+            }
+          },
+        },
+        KTPA: {
+          icao: "KTPA",
+          fullName: "Tampa International Airport",
+          metar: null,
+          parsedMetar: null,
+          configs: {
+            landing: {
+              N: "1L/1R",
+              S: "19L/19R",
+            },
+            departing: {
+              N: "1L/1R",
+              S: "19L/19R",
+            },
+          },
+          getLanding: function () {
+            if (this.parsedMetar.wind.speedKt < 5) {
+              return this.configs.landing.N;
+            } else {
+              if (
+                this.parsedMetar.wind.direction > 90 &&
+                this.parsedMetar.wind.direction <= 270
+              ) {
+                return this.configs.landing.S;
+              } else {
+                return this.configs.landing.N;
+              }
+            }
+          },
+          getDeparting: function () {
+            if (this.parsedMetar.wind.speedKt < 5) {
+              return this.configs.departing.N;
+            } else {
+              if (
+                this.parsedMetar.wind.direction > 90 &&
+                this.parsedMetar.wind.direction <= 270
+              ) {
+                return this.configs.departing.S;
+              } else {
+                return this.configs.departing.N;
+              }
+            }
+          },
+        },
+        KRSW: {
+          icao: "KRSW",
+          fullName: "Southwest Florida International Airport",
+          metar: null,
+          parsedMetar: null,
+          configs: {
+            landing: {
+              E: "6",
+              W: "24",
+            },
+            departing: {
+              E: "6",
+              W: "24",
+            },
+          },
+          getLanding: function () {
+            if (this.parsedMetar.wind.speedKt < 5) {
+              return this.configs.landing.E;
+            } else {
+              if (
+                this.parsedMetar.wind.direction >= 0 &&
+                this.parsedMetar.wind.direction < 180
+              ) {
+                return this.configs.landing.E;
+              } else {
+                return this.configs.landing.W;
+              }
+            }
+          },
+          getDeparting: function () {
+            if (this.parsedMetar.wind.speedKt < 5) {
+              return this.configs.departing.E;
+            } else {
+              if (
+                this.parsedMetar.wind.direction >= 0 &&
+                this.parsedMetar.wind.direction < 180
+              ) {
+                return this.configs.departing.E;
+              } else {
+                return this.configs.departing.W;
+              }
+            }
+          },
+        },
+        KPBI: {
+          icao: "KPBI",
+          fullName: "Palm Beach International Airport",
+          metar: null,
+          parsedMetar: null,
+          configs: {
+            landing: {
+              E: "10L/14",
+              W: "32/28R",
+            },
+            departing: {
+              E: "14/10L",
+              W: "28R/32",
+            },
+          },
+          getLanding: function () {
+            if (this.parsedMetar.wind.speedKt < 5) {
+              return this.configs.landing.E;
+            } else {
+              if (
+                this.parsedMetar.wind.direction >= 0 &&
+                this.parsedMetar.wind.direction < 180
+              ) {
+                return this.configs.landing.E;
+              } else {
+                return this.configs.landing.W;
+              }
+            }
+          },
+          getDeparting: function () {
+            if (this.parsedMetar.wind.speedKt < 5) {
+              return this.configs.departing.E;
+            } else {
+              if (
+                this.parsedMetar.wind.direction >= 0 &&
+                this.parsedMetar.wind.direction < 180
+              ) {
+                return this.configs.departing.E;
+              } else {
+                return this.configs.departing.W;
+              }
+            }
+          },
+        },
+        KEYW: {
+          icao: "KEYW",
+          fullName: "Key West International Airport",
+          metar: null,
+          parsedMetar: null,
+          configs: {
+            landing: {
+              E: "9",
+              W: "27",
+            },
+            departing: {
+              E: "9",
+              W: "27",
+            },
+          },
+          getLanding: function () {
+            if (this.parsedMetar.wind.speedKt < 5) {
+              return this.configs.landing.E;
+            } else {
+              if (
+                this.parsedMetar.wind.direction >= 0 &&
+                this.parsedMetar.wind.direction < 180
+              ) {
+                return this.configs.landing.E;
+              } else {
+                return this.configs.landing.W;
+              }
+            }
+          },
+          getDeparting: function () {
+            if (this.parsedMetar.wind.speedKt < 5) {
+              return this.configs.departing.E;
+            } else {
+              if (
+                this.parsedMetar.wind.direction >= 0 &&
+                this.parsedMetar.wind.direction < 180
+              ) {
+                return this.configs.departing.E;
+              } else {
+                return this.configs.departing.W;
+              }
+            }
+          },
+        },
+      },
+      numStationsLoaded: 0,
+    };
+  },
+  async mounted() {
+    await this.getWeatherForAirports();
+  },
+  methods: {
+    async getWeatherForAirports() {
+      for (const station of this.icao) {
+        const { data } = await zabApi.get(`/ids/stations/${station}`);
+        this.stations[station].metar = data.metar;
+        this.stations[station].parsedMetar = parse(data.metar);
+        this.numStationsLoaded++;
+      }
+    },
+    formatWind: function (station) {
+      if (station.parsedMetar.wind.speedKt < 4) return "Calm";
+      else if (!("speedKt" in station.parsedMetar.wind)) return "Unknown";
+      const paddedWind = `0${station.parsedMetar.wind.direction}`.slice(-3);
+      return `${paddedWind}@${station.parsedMetar.wind.speedKt}${
+        station.parsedMetar.wind.gust ? `G${station.parsedMetar.wind.gust}` : ""
+      }`;
+    },
+    getConditions: function (station) {
+      return station.parsedMetar.visibility.miles > 3
+        ? `<i class="material-icons weather_icon">wb_sunny</i>VFR`
+        : `<i class="material-icons weather_icon">wb_cloudy</i>IFR`;
+    },
+  },
 };
 </script>
 
