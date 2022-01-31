8b38c1d6d899d958c7c2ff733768c78335277fb9
Shane Friedman <shanekf2011@gmail.com>
2022-01-31 18:33:03 -0500

    Fixed a Shane Error

--- src/views/admin/controllers/Edit.vue
+++ src/views/admin/controllers/Edit.vue
@@ -84,7 +84,7 @@
 								class="cert cert_minor" 
 								@click="toggleCert">Minor Ground</span>
 							<span 
-								id="gnd" 
+								id="del" 
 								:class="{active: form.certs.del}" 
 								class="cert cert_minor" 
 								@click="toggleCert">Minor Clearance</span>
