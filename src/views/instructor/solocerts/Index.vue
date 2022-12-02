fe7574081b23559f91c8d0688007ba52cf7aed95
Tom Coombs <tom@cube74.com>
2022-12-02 09:06:23 -0500

    solo cert download

--- src/views/instructor/solocerts/Index.vue
+++ src/views/instructor/solocerts/Index.vue
@@ -69,7 +69,7 @@ export default {
 		async getSoloCerts() {
 			try {
 				//const {data} = await vatusaApi.get('/solo');
-				const {data} = fetch('https://api.vatusa.net/v2/solo');
+				const {data} = (await fetch('https://api.vatusa.net/v2/solo')).json();
 				for (const cert of data.data) {
 					//if(this.positions.includes(cert.position.slice(0, 3))) 
 						this.certs.push(cert);
