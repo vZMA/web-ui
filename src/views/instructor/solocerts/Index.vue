23fa322069f6a944c889debbee21dd5672410396
Tom Coombs <tom@cube74.com>
2022-12-02 13:40:45 -0500

    solocert display

--- src/views/instructor/solocerts/Index.vue
+++ src/views/instructor/solocerts/Index.vue
@@ -29,12 +29,12 @@
 							<div class="modal-content">
 								<h4>Delete solo certification?</h4>
 								<p>This will delete the solo certification for {{getName(cert.cid)}} on {{cert.position}} entirely.</p>
-							</div>
+							</div>-->
 							<div class="modal-footer">
 								<a href="#!" class="waves-effect btn" @click="deleteCert(cert.cid, cert.position)">Delete</a>
 								<a href="#!" class="modal-close waves-effect btn-flat">Cancel</a>
 							</div>
-						</div>-->
+						</div>
 					</tr>
 				</tbody>
 			</table>
