2c6281e6ccc4448ed572b68096df66205969d5de
Tom Coombs <tom@cube74.com>
2022-12-02 14:28:05 -0500

    delete solo cert

--- src/views/instructor/solocerts/Index.vue
+++ src/views/instructor/solocerts/Index.vue
@@ -24,8 +24,11 @@
 					<tr v-for="(cert, i) in certs" :key="cert.id">
 						<td><router-link :to="`/controllers/${cert.cid}`" class="controller_link">{{getName(cert.cid)}}</router-link></td>						<td>{{cert.position}}</td>
 						<td>{{cert.expires}}</td> 
-						
-						<!--<td class="options"><a :href="`#modal_delete_${i}`" class="modal-trigger red-text text-darken-2"><i class="material-icons">delete</i></a></td>
+						<td class="options">
+							<a :href="`#modal_delete_${i}`" data-position="top" data-tooltip="Delete Solocert" class="tooltipped modal-trigger">
+								<i class="material-icons red-text text-darken-2">delete</i>
+							</a>
+						</td>
 						<div :id="`modal_delete_${i}`" class="modal modal_delete">
 							<div class="modal-content">
 								<h4>Delete solo certification?</h4>
@@ -35,7 +38,7 @@
 								<a href="#!" class="waves-effect btn" @click="deleteCert(cert.cid, cert.position)">Delete</a>
 								<a href="#!" class="modal-close waves-effect btn-flat">Cancel</a>
 							</div>
-						</div>-->
+						</div>
 					</tr>
 				</tbody>
 			</table>
