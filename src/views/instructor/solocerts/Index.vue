313e744f15c6317e058e62640d6adf24e4e3bd4e
Tom Coombs <tom@cube74.com>
2022-12-02 13:38:27 -0500

    solocert display

--- src/views/instructor/solocerts/Index.vue
+++ src/views/instructor/solocerts/Index.vue
@@ -20,11 +20,11 @@
 						<th class="options">Options</th>
 					</tr>
 				</thead>
-<!--				<tbody class="certs_list_row">
+				<tbody class="certs_list_row">
 					<tr v-for="(cert, i) in certs" :key="cert.id">
 						<td><router-link :to="`/controllers/${cert.cid}`" class="controller_link">{{getName(cert.cid)}}</router-link></td>						<td>{{cert.position}}</td>
 						<td>{{cert.expires}}</td>
-						<td class="options"><a :href="`#modal_delete_${i}`" class="modal-trigger red-text text-darken-2"><i class="material-icons">delete</i></a></td>
+<!--						<td class="options"><a :href="`#modal_delete_${i}`" class="modal-trigger red-text text-darken-2"><i class="material-icons">delete</i></a></td>
 						<div :id="`modal_delete_${i}`" class="modal modal_delete">
 							<div class="modal-content">
 								<h4>Delete solo certification?</h4>
@@ -34,10 +34,10 @@
 								<a href="#!" class="waves-effect btn" @click="deleteCert(cert.cid, cert.position)">Delete</a>
 								<a href="#!" class="modal-close waves-effect btn-flat">Cancel</a>
 							</div>
-						</div>
+						</div>-->
 					</tr>
 				</tbody>
-			-->			</table>
+			</table>
 		</div>
 	</div>
 </template>
