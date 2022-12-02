b4f30cb77d6a8d8b0b4cf9b351d22521137158e5
Tom Coombs <tom@cube74.com>
2022-12-02 11:57:57 -0500

    solocert download

--- src/views/instructor/solocerts/Index.vue
+++ src/views/instructor/solocerts/Index.vue
@@ -72,7 +72,7 @@ export default {
 				console.log(data);
 				console.log(data.payload);
 				console.log(atob(data.payload));
-				const {data1} = JSON.parse(atob(data.payload));
+				const {data1} = atob(data.payload);
 				console.log(data1);
 				for (const cert of data1.data) {
 					//if(this.positions.includes(cert.position.slice(0, 3))) 
