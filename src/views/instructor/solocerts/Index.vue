b6c49339ae8dd8eb42916407735c1ab30e5fc26a
Tom Coombs <tom@cube74.com>
2022-12-02 21:37:15 -0500

    solo cert delete

--- src/views/instructor/solocerts/Index.vue
+++ src/views/instructor/solocerts/Index.vue
@@ -26,11 +26,11 @@
 						<td>{{cert.position}}</td>
 						<td>{{cert.expires}}</td> 
 						<td class="options">
-							<a :href="`#modal_delete_${i}`" data-position="top" data-tooltip="Delete Solocert" class="tooltipped modal-trigger">
+							<a :href="`#modal_delete_${cert.id}`" data-position="top" data-tooltip="Delete Solocert" class="tooltipped modal-trigger">
 								<i class="material-icons red-text text-darken-2">delete</i>
 							</a>
 						</td>
-						<div :id="`modal_delete_${i}`" class="modal modal_delete">
+						<div :id="`modal_delete_${cert.id}`" class="modal modal_delete">
 							<div class="modal-content">
 								<h4>Delete solo certification?</h4>
 								<p>This will delete the solo certification for <!--{{getName(cert.cid)}} on {{cert.position}}--> entirely.</p>
