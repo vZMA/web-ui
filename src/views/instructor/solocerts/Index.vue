58714b57251ed85199a03e47ed8990cd7dda6bbc
Tom Coombs <tom@cube74.com>
2022-12-03 16:53:02 -0500

    solocerts testing

--- src/views/instructor/solocerts/Index.vue
+++ src/views/instructor/solocerts/Index.vue
@@ -10,7 +10,7 @@
         <div class="loading_container" v-if="loading">
             <Spinner />
         </div>
-        <div class="no_certs" v-else-if="loading === false && certs.length === 0"><p>There are no active solo certifications issued by ZMA</p></div>
+        <div class="no_certs" v-else-if="certs.length === 0"><p>There are no active solo certifications issued by ZMA</p></div>
         <div class="certs_wrapper" v-else>
 			 <table class="certs_list striped compact" >
                 <thead class="certs_list_head">
