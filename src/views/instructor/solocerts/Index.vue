a7b8a2db5d8896718f59de6c0e6c6505a6004659
Tom Coombs <tom@cube74.com>
2022-12-03 09:19:14 -0500

    solo cert delete

--- src/views/instructor/solocerts/Index.vue
+++ src/views/instructor/solocerts/Index.vue
@@ -3,51 +3,52 @@
 		<div class="card-content">
 			<div class="row row_no_margin">
 				<div class="card-title col s8"><span class="card-title">Solo Certifications</span></div>
-				<div class="col s4"><router-link to="/ins/solo/new"><span class="btn new_event_button right">New</span></router-link></div>
+				<div class="col s4"><router-link to="/admin/events/new"><span class="btn new_event_button right">New</span></router-link></div>
 			</div>
 		</div>
-		<div class="card-content loading_container" v-if="loading">
+		<div class="loading_container" v-if="!events">
 			<Spinner />
 		</div>
-		<p class="no_certs" v-else-if="loading === false && certs.length === 0">There are no active solo certifications issued by ZMA</p>
-		<div class="table_wrapper" v-else>
-			<table class="striped compact">
-				<thead class="certs_list_head">
-					<tr>
-						<th>Controller</th>
-						<th>Position</th>
-						<th>Expires</th>
-						<th class="options">Options</th>
-					</tr>
-				</thead>
-				<tbody class="certs_list_row">
-					<tr v-for="cert in certs" :key="cert.id">
-						<td><router-link :to="`/controllers/${cert.cid}`" class="controller_link">{{getName(cert.cid)}}</router-link></td>
-						<td>{{cert.position}}</td>
-						<td>{{cert.expires}}</td> 
-						<td class="options">
-							<a :href="`#modal_delete_${cert.id}`" data-position="top" data-tooltip="Delete Solocert" class="tooltipped modal-trigger">
-								<i class="material-icons red-text text-darken-2">delete</i>
-							</a>
-						</td>
-						<div :id="`modal_delete_${cert.id}`" class="modal modal_delete">
-							<div class="modal-content">
-								<h4>Delete solo certification?</h4>
-								<p>This will delete the solo certification for <!--{{getName(cert.cid)}}--> on {{cert.position}} entirely.</p>
-							</div>
-							<div class="modal-footer">
-								<a href="#!" class="waves-effect btn" @click="deleteCert(cert.cid, cert.position)">Delete</a>
-								<a href="#!" class="modal-close waves-effect btn-flat">Cancel</a>
-							</div>
+		<p class="no_certs" v-else-if="loading === false && certs.length == 0">There are no active solo certifications issued by ZMA</p>
+		<table class="certs_list striped compact" v-else>
+			<thead class="certs_list_head">
+				<tr>
+					<th>Controller</th>
+					<th>Postition</th>
+					<th>Expires</th>
+					<th class="options">Options</th>
+				</tr>
+			</thead>
+			<tbody class="certs_list_row">
+				<tr v-for="(cert, i) in certs" :key="cert.id">
+					<td><router-link :to="`/controllers/${cert.cid}`" class="controller_link">{{getName(cert.cid)}}</router-link>></td>
+					<td>{{cert.position}}</td>
+					<td>{{cert.expires}}</td>
+					<td class="options">
+						<a :href="`#modal_delete_${i}`" data-position="top" data-tooltip="Delete Solo Cert" class="tooltipped modal-trigger">
+							<i class="material-icons red-text text-darken-2">delete</i>
+						</a>
+					</td>
+					<div :id="`modal_delete_${i}`" class="modal modal_delete">
+						<div class="modal-content">
+							<h4>Delete Solo Certificate?</h4>
+							<p>This will delete the solo certification on VATUSA.  Are you sure?</p>
 						</div>
-					</tr>
-				</tbody>
-			</table>
-		</div>
+						<div class="modal-footer">
+							<a href="#!" class="modal-close waves-effect waves-light btn" @click="deleteCert(cert.cid, cert.position)">Delete</a>
+							<a href="#!" class="modal-close waves-effect waves-light btn-flat">Cancel</a>
+						</div>
+					</div>
+				</tr>
+			</tbody>
+		</table>
 	</div>
+	<Past />
 </template>
+
 <script>
-import {vatusaApiAuth, vatusaApi, zabApi} from '@/helpers/axios.js';
+import { zabApi } from '@/helpers/axios.js';
+import Past from './Past.vue';
 
 export default {
 	name: 'SoloCerts',
@@ -60,69 +61,83 @@ export default {
 			loading: true
 		};
 	},
+	
 	async mounted() {
 		await this.getSoloCerts();
 		await this.getControllers();
-		this.loading = false;
-		
+
 		M.Modal.init(document.querySelectorAll('.modal'), {
 			preventScrolling: false
 		});
-		M.Tooltip.init(document.querySelectorAll('.tooltipped'), { 
+		M.Tooltip.init(document.querySelectorAll('.tooltipped'), {
 			margin: 0
 		});
-
+		this.loading = false;
 	},
 	methods: {
-		async getSoloCerts() {
-			try {
-				const {data} = await vatusaApi.get('/solo');
-				const payload = atob(data.payload);
-				var data1 = JSON.parse(payload);  
-				for (const cert of data1.data) {
-					//if(this.positions.includes(cert.position.slice(0, 3))) 
-						this.certs.push(cert);
+	async getSoloCerts() {
+				try {
+					const {data} = await vatusaApi.get('/solo');
+					const payload = atob(data.payload);
+					var data1 = JSON.parse(payload);  
+					for (const cert of data1.data) {
+						//if(this.positions.includes(cert.position.slice(0, 3))) 
+							this.certs.push(cert);
+					}
+				} catch(e) {
+					console.log(e);  
 				}
-			} catch(e) {
-				console.log(e);  
-			}
-		},
-		async getControllers() {
-			try {
-				const {data} = await zabApi.get('/feedback/controllers');
-				this.controllers = data.data;
-			} catch(e) {
-				console.log(e);
-			}
-		},
-		async deleteCert(cid, pos) {
-			try {
-				const formData = new FormData();
-				formData.append('cid', cid);
-				formData.append('position', pos);
-				await vatusaApiAuth.delete('/solo', formData);
+			},
+			async getControllers() {
+				try {
+					const {data} = await zabApi.get('/feedback/controllers');
+					this.controllers = data.data;
+				} catch(e) {
+					console.log(e);
+				}
+			},
+			async deleteCert(cid, pos) {
+				try {
+					const formData = new FormData();
+					formData.append('cid', cid);
+					formData.append('position', pos);
+					await vatusaApiAuth.delete('/solo', formData);
 
-				this.toastSuccess('Solo Certification deleted');
+					this.toastSuccess('Solo Certification deleted');
 
-				await this.getSoloCerts();
-				this.$nextTick(() => {
-					M.Modal.getInstance(document.querySelector('.modal_delete')).close();
-				});
-				
-			} catch(e) {
-				this.toastError('Something went wrong, please try again');
+					await this.getSoloCerts();
+					this.$nextTick(() => {
+						M.Modal.getInstance(document.querySelector('.modal_delete')).close();
+					});
+					
+				} catch(e) {
+					this.toastError('Something went wrong, please try again');
+				}
+			},
+			getName(cid2) {
+				const controller = this.controllers.filter(i => { return i.cid === cid2; });
+				console.log(controller);
+				return 'Mickey Mouse'; //controller[0].fname + ' ' + controller[0].lname;
 			}
-		},
-		getName(cid2) {
-			const controller = this.controllers.filter(i => { return i.cid === cid2; });
-			console.log(controller);
-			return 'Mickey Mouse'; //controller[0].fname + ' ' + controller[0].lname;
 		}
-	}
-};
+	};
 </script>
 
 <style scoped lang="scss">
+.name {
+	color: $primary-color;
+	font-weight: 700;
+}
+
+table tbody {
+	tr {
+		transition: background-color .3s ease;
+		&:hover {
+			background: #eaeaea;
+		}
+	}
+}
+
 .no_certs {
 	padding: 0 1em 1em 1em;
 	margin-top: -1em;
@@ -139,12 +154,6 @@ export default {
 	text-align: center;
 	margin: auto;
 }
-
-.modal_delete {
-	min-width: 400px;
-	width: 30%;
-}
-
 .controller_link {
 	font-weight: 700;
 	color: $primary-color;
@@ -152,5 +161,10 @@ export default {
 	&:hover {
 		color: $primary-color-light;
 	}
+}
+
+.modal_delete {
+	min-width: 400px;
+	width: 30%;
 }
 </style>
