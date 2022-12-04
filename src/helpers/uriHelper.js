1b261436a0ba9647a038f6ad7e4abf0a7648352e
Tom Coombs <tom@cube74.com>
2022-12-04 14:44:39 -0500

    OAUTH CHANGES

--- src/helpers/uriHelper.js
+++ src/helpers/uriHelper.js
@@ -5,17 +5,10 @@ export const vatsimAuthRedirectUrl = `${getAuthEndpoint()}/oauth/authorize?clien
   export const discordRedirectUrl = `https://discord.com/api/oauth2/authorize?client_id=546531280842653696&redirect_uri=https%3A%2F%2Fzmaartcc.net%2Fconnect%2Fdiscord&response_type=code&scope=identify`;
   
   function getClientId() {
-    const betaClientId = 1167;
-    const prodClientId = 1171;
-    const devClientId = 466;
-  
-    if (window.location.origin.includes("beta")) {
-      return betaClientId;
-    } else if (window.location.origin.includes("zmaartcc.net")) {
-      return prodClientId;
-    } else { 
-      return devClientId;
-    }
+    const ClientId = 1186;
+    
+  return prodClientId;
+    
   }
   
   function getAuthEndpoint() {
