d268890a43b7cf600e0b0079137fa8cb3e3150b9
Tom Coombs <tom@cube74.com>
2022-12-04 18:48:14 -0500

    background and privacy policy edit

--- src/views/page/PrivacyPolicy.vue
+++ src/views/page/PrivacyPolicy.vue
@@ -5,9 +5,14 @@
 			<p>
 				As a guest, the Miami ARTCC does not collect any data about you, except:
 				<ul class="browser-default">
-					<li>when you log in with your VATSIM account. Some personal information is shared with us, including, but not limited to, your name, email address, VATSIM ID and rating. You are not required to log in to access most parts of our website.</li>
-					<li>when you submit feedback on our website. In this case, we'll only collect the data you choose to submit through the form. Your name, email address, and VATSIM ID are always collected and shared with the ATM, DATM, and TA. You'll also need to log in through VATSIM to submit feedback.</li>
-					<li>when you encounter an error on our website. In this case, we'll only collect basic, non-identifiable information about your browser, OS, and the error you encountered.</li>
+					<li>when you log in with your VATSIM account. Some personal information is shared with us, including, but not limited to, 
+						your name, email address, VATSIM ID and rating. You are not required to log in to access most parts of our website.</li>
+					<li>when you submit feedback on our website. In this case, we'll only collect the data you choose to submit through the form.
+						Your name, email address, and VATSIM ID are always collected and shared with the ATM, DATM, and TA. You'll
+						 
+						also need to log in through VATSIM to submit feedback.</li>
+					<li>when you encounter an error on our website. In this case, we'll only collect basic, non-identifiable
+						information about your browser, OS, and the error you encountered.</li>
 				</ul>
 			</p>
 			<p>
@@ -19,10 +24,15 @@
 				We may share this information with VATSIM and other subdivisions within VATUSA.
 			</p>
 			<p>
-				We do not store any cookies on your computer, except for functional cookies that are needed to make our website work, like identification tokens. You may choose to disable cookies through your browser at the cost of being able to log in.
+				We do not store any cookies on your computer, except for functional cookies that are needed to make our website work, 
+				like identification tokens. You may choose to disable cookies through your browser at the cost of being able to log in.
 			</p>
 			<p>
-				Given the nature of our services, it is not possible to opt out of data collection and use our services. But if you desire to opt out and no longer use our services, we will purge all information we have collected upon written request. The first step is to deactivate and request VATSIM to purge your data. Please head to <a href="https://membership.vatsim.net/" noopener noreferrer target="_blank">https://membership.vatsim.net/</a> to do so. VATSIM, after processing your opt out, will forward the request to VATUSA and the Albuquerque ARTCC for processing.
+				Given the nature of our services, it is not possible to opt out of data collection and use our services. But if you 
+				desire to opt out and no longer use our services, we will purge all information we have collected upon written request. 
+				The first step is to deactivate and request VATSIM to purge your data. Please head to
+				<a href="https://membership.vatsim.net/" noopener noreferrer target="_blank">https://membership.vatsim.net/</a> to do so. VATSIM, after 
+				processing your opt out, will forward the request to VATUSA and the Maimi ARTCC for processing.
 			</p>
 		</div>
 	</div>
