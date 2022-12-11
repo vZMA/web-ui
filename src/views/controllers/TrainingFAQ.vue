007842b1f74b2108d9aae7fbb167d76d364019af
Tom Coombs <tom@cube74.com>
2022-12-11 09:47:19 -0500

    trainingFAQ formatting

--- src/views/controllers/TrainingFAQ.vue
+++ src/views/controllers/TrainingFAQ.vue
@@ -4,39 +4,40 @@
 			<span class="card-title">Frequently Asked Questions</span>
 			<p>
                 <p>These are some of the questions we receive from new ARTCC members regarding traing and first steps</p>
-                <li>
-                    <p><b>I am new to the Miami ARTCC. What do I do?</b></p>
-                    <li>If you are an Observer (OBS), you can begin requesting training through your Controller Dashboard. You will first undergo the 
-                        New Student Orientation training and then we will begin on your Clearance Delivery/Ground Control (S1) rating course material.</li>                  
-                    </li>
-                    <li>
-                        If you just transferred to the Miami ARTCC, you need to complete the <a href="https://academy.vatusa.net/course/view.php?id=50" target="_blank">Orientation course</a> and pass the examination. 
-                        Once you do that, you need to request a GRP Competency Check through your Controller Dashboard.
-                    </li>
-                <li><p><b>What will I learn?</b></p>
-                    <li>
-                        Throughout your training, you'll be introduced to many concepts. To learn what you will learn, please view the 
-                        appropriate <a href="https://docs.google.com/document/d/1CS8ivM2lUssXd2bm_Z9Ex5OGJNqMpTqOU9hpAPWbjro/edit?usp=sharing" target="_blank">Training Program</a> document.
-                    </li>
-                </li>
-                <li>
-                    <p><b>How do I request training?</b></p>
+                <ul>
+                    <li><b>I am new to the Miami ARTCC. What do I do?</b>
+                            <li>If you are an Observer (OBS), you can begin requesting training through your Controller Dashboard. You will first undergo the 
+                                New Student Orientation training and then we will begin on your Clearance Delivery/Ground Control (S1) rating course material.</li>                  
+                            </li>
+                            <li>
+                                If you just transferred to the Miami ARTCC, you need to complete the <a href="https://academy.vatusa.net/course/view.php?id=50" target="_blank">Orientation course</a> and pass the examination. 
+                                Once you do that, you need to request a GRP Competency Check through your Controller Dashboard.
+                            </li>
+                    <li><b>What will I learn?</b>
                         <li>
-                        You can request training through your Controller Dashboard on the Miami ARTCC website. You access the Controller Dashboard 
-                        by logging in, clicking your name (on the upper right-hand side of the page), and clicking Controller Dashboard. You then go to Training Sessions and then 
-                        click Request to add your availability. Remember, this time is in UTC/Zulu time.
+                            Throughout your training, you'll be introduced to many concepts. To learn what you will learn, please view the 
+                            appropriate <a href="https://docs.google.com/document/d/1CS8ivM2lUssXd2bm_Z9Ex5OGJNqMpTqOU9hpAPWbjro/edit?usp=sharing" target="_blank">Training Program</a> document.
                         </li>
-                </li>
-                <li>
-                    <p><b>How do I see my Training Records?</b></p>
-                    <li>You can see your Training Records by going to the Training Sessions portion of your Controller Dashboard and clicking the Training Session you wish to review.</li>
-                    <li>As an alternative, you can view your Training Records by visiting the VATUSA website, logging in, and then clicking My VATUSA, profile, and then clicking the Training Record tab.</li>
-                </li>
-                <li><p><b>I need training help!</b></p>
-                    <li>
-                    If you need help with your training or if you have an Air Traffic Control question, please email the Training Team at <a href="mailto:training@zmaartcc.net">training@zmaartcc.net</a>.
                     </li>
-                </li>
+                    <li>
+                        <b>How do I request training?</b>
+                            <li>
+                            You can request training through your Controller Dashboard on the Miami ARTCC website. You access the Controller Dashboard 
+                            by logging in, clicking your name (on the upper right-hand side of the page), and clicking Controller Dashboard. You then go to Training Sessions and then 
+                            click Request to add your availability. Remember, this time is in UTC/Zulu time.
+                            </li>
+                    </li>
+                    <li>
+                        <b>How do I see my Training Records?</b>
+                        <li>You can see your Training Records by going to the Training Sessions portion of your Controller Dashboard and clicking the Training Session you wish to review.</li>
+                        <li>As an alternative, you can view your Training Records by visiting the VATUSA website, logging in, and then clicking My VATUSA, profile, and then clicking the Training Record tab.</li>
+                    </li>
+                    <li><b>I need training help!</b>
+                        <li>
+                        If you need help with your training or if you have an Air Traffic Control question, please email the Training Team at <a href="mailto:training@zmaartcc.net">training@zmaartcc.net</a>.
+                        </li>
+                    </li>
+                </ul>
             </p>
 		</div>
 	</div>
