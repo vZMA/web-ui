5e20eeb25c1cb5940ac4dc66aaf5c9477d3799eb
Tom Coombs <tom@cube74.com>
2022-12-11 09:51:17 -0500

    TrainingFAQ format changes

--- src/views/controllers/TrainingFAQ.vue
+++ src/views/controllers/TrainingFAQ.vue
@@ -6,6 +6,7 @@
                 <p>These are some of the questions we receive from new ARTCC members regarding traing and first steps</p>
                 <ul>
                     <li><b>I am new to the Miami ARTCC. What do I do?</b>
+                        <ul>
                             <li>If you are an Observer (OBS), you can begin requesting training through your Controller Dashboard. You will first undergo the 
                                 New Student Orientation training and then we will begin on your Clearance Delivery/Ground Control (S1) rating course material.</li>                  
                             </li>
@@ -13,29 +14,38 @@
                                 If you just transferred to the Miami ARTCC, you need to complete the <a href="https://academy.vatusa.net/course/view.php?id=50" target="_blank">Orientation course</a> and pass the examination. 
                                 Once you do that, you need to request a GRP Competency Check through your Controller Dashboard.
                             </li>
+                        </ul>
                     <li><b>What will I learn?</b>
-                        <li>
-                            Throughout your training, you'll be introduced to many concepts. To learn what you will learn, please view the 
-                            appropriate <a href="https://docs.google.com/document/d/1CS8ivM2lUssXd2bm_Z9Ex5OGJNqMpTqOU9hpAPWbjro/edit?usp=sharing" target="_blank">Training Program</a> document.
-                        </li>
+                        <ul>
+                            <li>
+                                Throughout your training, you'll be introduced to many concepts. To learn what you will learn, please view the 
+                                appropriate <a href="https://docs.google.com/document/d/1CS8ivM2lUssXd2bm_Z9Ex5OGJNqMpTqOU9hpAPWbjro/edit?usp=sharing" target="_blank">Training Program</a> document.
+                            </li>
+                        </ul>
                     </li>
                     <li>
                         <b>How do I request training?</b>
+                        <ul>
                             <li>
-                            You can request training through your Controller Dashboard on the Miami ARTCC website. You access the Controller Dashboard 
-                            by logging in, clicking your name (on the upper right-hand side of the page), and clicking Controller Dashboard. You then go to Training Sessions and then 
-                            click Request to add your availability. Remember, this time is in UTC/Zulu time.
+                                You can request training through your Controller Dashboard on the Miami ARTCC website. You access the Controller Dashboard 
+                                by logging in, clicking your name (on the upper right-hand side of the page), and clicking Controller Dashboard. You then go to Training Sessions and then 
+                                click Request to add your availability. Remember, this time is in UTC/Zulu time.
                             </li>
+                        </ul>
                     </li>
                     <li>
                         <b>How do I see my Training Records?</b>
-                        <li>You can see your Training Records by going to the Training Sessions portion of your Controller Dashboard and clicking the Training Session you wish to review.</li>
-                        <li>As an alternative, you can view your Training Records by visiting the VATUSA website, logging in, and then clicking My VATUSA, profile, and then clicking the Training Record tab.</li>
+                        <ul>
+                            <li>You can see your Training Records by going to the Training Sessions portion of your Controller Dashboard and clicking the Training Session you wish to review.</li>
+                            <li>As an alternative, you can view your Training Records by visiting the VATUSA website, logging in, and then clicking My VATUSA, profile, and then clicking the Training Record tab.</li>
+                        </ul>
                     </li>
                     <li><b>I need training help!</b>
-                        <li>
-                        If you need help with your training or if you have an Air Traffic Control question, please email the Training Team at <a href="mailto:training@zmaartcc.net">training@zmaartcc.net</a>.
-                        </li>
+                        <ul>
+                            <li>
+                            If you need help with your training or if you have an Air Traffic Control question, please email the Training Team at <a href="mailto:training@zmaartcc.net">training@zmaartcc.net</a>.
+                            </li>
+                        </ul>
                     </li>
                 </ul>
             </p>
