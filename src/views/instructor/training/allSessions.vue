2d490bb4e08767c5c84ca699a29a353cbb0c1c92
Tom Coombs <tom@cube74.com>
2022-12-12 07:56:27 -0500

    allsessions testing

--- src/views/instructor/training/allSessions.vue
+++ src/views/instructor/training/allSessions.vue
@@ -23,7 +23,7 @@
 				</thead>
 				<tbody class="sessions_list_row">
 					<tr v-for="(session, i) in sessions" :key="session._id">
-						<td>{{session.student.fname + ' ' + session.student.lname}} <span v-if="session.student.vis === true">(VC)</span></td>
+						<td>{{session.student.fname + ' ' + session.student.lname}} <!--<span v-if="session.student.vis === true">(VC)</span>--></td>
 						<td>{{session.milestone.name}}</td>
 						<td>{{dtLong(session.startTime)}}</td>
 						<td>{{dtLong(session.endTime)}}</td>
