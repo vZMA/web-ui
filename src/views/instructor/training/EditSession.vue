2d4e8c37c62010d5434c212d43487ae00233c359
Tom Coombs <tom@cube74.com>
2023-01-02 08:50:09 -0500

    logging added to edit sessions

--- src/views/instructor/training/EditSession.vue
+++ src/views/instructor/training/EditSession.vue
@@ -252,13 +252,14 @@ export default {
 		},
 		decreaseTime(type) {
 			if(type === 'end' && new Date(this.session.endTime)-900000 > new Date(this.session.startTime)) {
+				console.log(`end time decreased`);
 				let d = new Date(this.session.endTime);
 				d.setUTCMinutes(d.getUTCMinutes()-15);
 				this.session.endTime = d.toISOString();		
 			} else if(type === 'start') {
-				console.log(`decrease start time`);
+				console.log('start time decreased');
 				let d = new Date(this.session.startTime);
-				d.setUTCMinutes(d.getUTCMinutes() - 15);
+				d.setUTCMinutes(d.getUTCMinutes()-15);
 				this.session.startTime = d.toISOString();
 			}
 		}
