509245d9a69a4063643c7e4ad6ee696b764e6312
Tom Coombs <tom@cube74.com>
2023-03-30 19:06:50 -0400

    event page and summary timezone fix

--- src/views/events/Index.vue
+++ src/views/events/Index.vue
@@ -15,7 +15,7 @@
 				<div class="row">
 					<div class="col s12 l8">
 						<span class="card-title event_title">{{event.name}}</span>
-						<span class="card-title event_date">{{dtLong(event.eventStart)}} <i class="material-icons rotate tiny">airplanemode_active</i> {{formatTime(event.eventEnd)}}z</span>
+						<span class="card-title event_date">{{dtLong(event.eventStart)}} <i class="material-icons rotate tiny">airplanemode_active</i> {{formatTime(event.eventEnd)}}</span>
 					</div>
 					<div class="col s12 l4">
 						<router-link :to="`/events/${event.url}`" class="btn btn-signup waves-effect waves-light right">More Info &amp; Sign Up</router-link>
