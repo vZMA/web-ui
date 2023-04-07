fc8a09b6e62cc8dc5f5e13c536053cb7a86da41f
Tom Coombs <tom@cube74.com>
2023-04-06 23:38:35 -0400

    oauth2 uri test

--- src/helpers/uriHelper.js
+++ src/helpers/uriHelper.js
@@ -1,6 +1,6 @@
 export const vatsimAuthRedirectUrl = `${getAuthEndpoint()}/oauth/authorize?client_id=${getClientId()}&redirect_uri=${
-   //"http://dev.zmaartcc.net/login/verify"
-   window.location.origin + "/login/verify"
+   "https://zmaartcc.net/login/verify"
+   //window.location.origin + "/login/verify"
   }&response_type=code&scope=vatsim_details+full_name+email`;
   
   export const discordRedirectUrl = `https://discord.com/api/oauth2/authorize?client_id=546531280842653696&redirect_uri=https%3A%2F%2Fzmaartcc.net%2Fconnect%2Fdiscord&response_type=code&scope=identify`;
