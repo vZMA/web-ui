7468c3f2eb7d2ed49b1d615fb383acf5424b0d91
Tom Coombs <tom@cube74.com>
2024-03-05 13:44:53 -0500

    zmaartcc pointed to infotool.zmaartcc.net

--- src/views/partial/Header.vue
+++ src/views/partial/Header.vue
@@ -58,9 +58,9 @@
             <ul id="files-dropdown" class="dropdown-content">
               <li>
                 <a
-                  href="https://zmaartcc.net/chartsindex.html"
+                  href="https://infotool.zmaartcc.net/"
                   target="_blank"
-                  rel="noopener noreferrer">ZMA Charts</a>
+                  rel="noopener noreferrer">ZMA Charts and Info</a>
               </li>
               <!--<li>
                 <router-link to="/files/documents">Documents</router-link>
