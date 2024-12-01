f35d588a278f4367ab75448575401cae265e97be
Jason Cochran <jason.cochran@gmail.com>
2024-11-30 21:19:13 -0500

    minor formatting and spelling

--- src/views/controllers/WhatWeDo.vue
+++ src/views/controllers/WhatWeDo.vue
@@ -22,27 +22,27 @@
 	</div>
     <div class="card">
 		<div class="card-content">
-			<span class="card-title">TESTAMONIALS</span>
+			<span class="card-title">TESTIMONIAL</span>
 			<p>Please read the passionate words of one of our young participants below who joined the network as a high school student and has parlayed that into a career in aviation.</p>
             <p>A few years ago, I decided to give VATSIM a try for the first time after one of my friends convinced me to download it so that we could start flying online together. Admittedly, I was a little hesitant at the time. I was entirely unaware of the life-changing effects this network would ultimately have on me. Fast forward a few months; I was hooked on VATSIM. I would fly large airliners into bustling airports following real world procedures while making the best use of my time to understand complex instrument procedures that I may have never discovered or practiced otherwise. </p>
             <p>One day I thought to myself, “If I’ve already learned this much just from flying on the network, why not take it one step further and learn how to be a controller?” Maybe then I would finally get some clarity as to what’s actually happening behind the scenes. Without delay, I studied basic air traffic concepts at VATUSA's Academy and decided I would join the Miami ARTCC. I didn’t know it at the time, but this was one of the best decisions I’ve ever made in my life. </p>
             <p>As I progressed through my VATSIM ATC studies at Miami, I quickly met some of the most talented and passionate individuals who truly displayed a love for and knowledge of aviation, both as pilots and controllers. Many of these people already were, or have gone on to become, real-world pilots and air traffic controllers. These people are now close friends whom I interact with on an almost a daily basis. I honestly couldn’t have asked for a better group of people to share my passion for aviation with. </p>
-            <p>Today, I am a real world instrument-rated commercial pilot working toward a job with the airlines, I can say with 100% certainty that participating on VATSIM was one of the best choices I made to support my real-world pilot training:
+            <p>Today, I am a real world instrument-rated commercial pilot working toward a job with the airlines, I can say with 100% certainty that participating on VATSIM was one of the best choices I made to support my real-world pilot training:</p>
             <ul>
-                <li><p>I gained an abundance of aviation knowledge that I would not have learned from my flight school, or even by flying many hours in a real airplane because there’s a chance I would not be exposed to the same scenarios.
-                </p></li>
-                <li><p>I gained a deep understanding of how the National Airspace System actually functions, and I know what is happening “on the other side of the mic.” 
-                </p></li>
-                <li><p>I am a safer and better pilot because of this knowledge and context. 
-                </p></li>
-                <li><p>I received compliments from my real-world aviation instructors who were perplexed by the amount of aviation knowledge I obtained and my expertise on various topics. 
-                </p></li>
-                <li><p>I was able to fly many real-world cross-country flights, into some of the busiest airspace in the United States, with ease and confidence the entire time because I know exactly what’s happening around me. 
-                </p></li>
-                <li><p>I have put my full faith into the knowledge I’ve gained over the years (a significant portion of which came from participation in the VATSIM community) and it has never failed me. 
-                </p></li>
+                <li>I gained an abundance of aviation knowledge that I would not have learned from my flight school, or even by flying many hours in a real airplane because there’s a chance I would not be exposed to the same scenarios.
+                </li>
+                <li>I gained a deep understanding of how the National Airspace System actually functions, and I know what is happening “on the other side of the mic.” 
+                </li>
+                <li>I am a safer and better pilot because of this knowledge and context. 
+                </li>
+                <li>I received compliments from my real-world aviation instructors who were perplexed by the amount of aviation knowledge I obtained and my expertise on various topics. 
+                </li>
+                <li>I was able to fly many real-world cross-country flights, into some of the busiest airspace in the United States, with ease and confidence the entire time because I know exactly what’s happening around me. 
+                </li>
+                <li>I have put my full faith into the knowledge I’ve gained over the years (a significant portion of which came from participation in the VATSIM community) and it has never failed me. 
+                </li>
 
-            </ul></p>
+            </ul>
             <p>I do not believe I would be nearly as knowledgeable or safe of an aviator as I am today without VATSIM, and I owe it to those who’ve volunteered their time, knowledge, and energy to support me during my journey. </p>
             <p>For anyone considering joining VATSIM, please know how much I encourage you to at least dip your toes into the water. It’s free, and low commitment, and it just might change your life or your career trajectory. It’s not a video game- it’s a community where aviation enthusiasts and practitioners come together to deepen their understanding of the career or hobby they love so much.</p>
             <p>It has been my privilege to make lifelong friends who inspire me to be the best I can. I wouldn’t be even half the aviator I am today without such a supportive network to learn on. I still go back and thank that friend who first introduced me to VATSIM. I have no regrets, and I’m confident anyone serious enough about learning aviation either as a career or hobby, who really puts their time and effort into learning how to fly or control on VATSIM, will have no regrets either.</p>
