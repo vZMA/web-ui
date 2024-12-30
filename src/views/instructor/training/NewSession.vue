0232a4bcdbd28a08200bf3989f279f3426a580c2
tom@cube74.com <tom@cube74.com>
2024-12-30 16:51:46 -0500

    new sessions addition

--- src/views/instructor/training/NewSession.vue
+++ src/views/instructor/training/NewSession.vue
@@ -172,7 +172,7 @@ export default {
 						});
 					if(data.ret_det.code === 200) {
 						this.toastSuccess('Session created');
-						this.$router.push('/ins/sessions');
+						this.$router.push('/ins/training/sessions');
 					} else {
 						this.toastError(data.ret_det.message);
 					}
