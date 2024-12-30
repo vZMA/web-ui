0197b646476ba383d31d060888a4cb077bd5e95d
tom@cube74.com <tom@cube74.com>
2024-12-30 15:46:02 -0500

    update to send milestone

--- src/views/instructor/training/NewSession.vue
+++ src/views/instructor/training/NewSession.vue
@@ -159,6 +159,7 @@ export default {
 					const {data} = await zabApi.post(`/training/session/new`, {
 						studentCid: this.session.studentCid,
 						instructorCid: this.session.instructorCid,
+						milestoneCode: this.session.milestone.
 						position: this.session.position,
 						movements: this.session.movements,
 						progress: this.session.progress,
