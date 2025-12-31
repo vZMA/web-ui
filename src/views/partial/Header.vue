9dc10b452e9721cfce284a0574711988829a56f7
tom@cube74.com <tom@cube74.com>
2025-12-30 20:24:02 -0500

    update sidebar

--- src/views/partial/Header.vue
+++ src/views/partial/Header.vue
@@ -160,13 +160,12 @@
         <router-link to="/controllers/staff">STAFF</router-link>
       </li>
       <li>
-              <a
+          <a
                   v-show="user.isLoggedIn"
                   href="https://sites.google.com/view/miami-artcc-training-academy"
                   target="_blank"
                   rel="noopener noreferrer">TRAINING ACADEMY</a>
-              </li>
-          <li>
+      </li>
       <li>
         <router-link class="sidenav-close" to="/events">EVENTS</router-link>
       </li>
