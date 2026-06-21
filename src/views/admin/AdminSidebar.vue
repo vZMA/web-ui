3a3e0b7edea733c8774b1cd31b78a0822a7b8401
tom@cube74.com <tom@cube74.com>
2026-06-20 20:42:36 -0400

    perms updates

--- src/views/admin/AdminSidebar.vue
+++ src/views/admin/AdminSidebar.vue
@@ -9,7 +9,7 @@
 				Controllers
 				<div class="secondary-content"><i class="material-icons">people</i></div>
 			</router-link>
-			<router-link to="/admin/activity" class="collection-item" v-if="requiresAuth(['atm', 'datm', 'ta', 'fe', 'wm'])">
+			<router-link to="/admin/activity" class="collection-item" v-if="requiresAuth(['atm', 'datm', 'ata', 'ta', 'afe', 'fe', 'awm', 'wm'])">
 				Controller Activity
 				<div class="secondary-content"><i class="material-icons">assessment</i></div>
 			</router-link>
@@ -21,11 +21,11 @@
 				Downloads
 				<div class="secondary-content"><i class="material-icons">insert_drive_file</i></div>
 			</router-link>
-			<router-link to="/admin/events" class="collection-item" v-if="requiresAuth(['atm', 'datm', 'ta', 'ec', 'wm'])">
+			<router-link to="/admin/events" class="collection-item" v-if="requiresAuth(['atm', 'datm', 'ta', 'ata', 'aec', 'ec', 'awm','wm'])">
 				Events
 				<div class="secondary-content"><i class="material-icons">event</i></div>
 			</router-link>
-			<router-link to="/admin/feedback" class="collection-item" v-if="requiresAuth(['atm', 'datm', 'ta', 'wm'])">
+			<router-link to="/admin/feedback" class="collection-item" v-if="requiresAuth(['atm', 'datm', 'ata', 'ta', 'wm'])">
 				Feedback
 				<div class="secondary-content"><i class="material-icons">feedback</i></div>
 			</router-link>
